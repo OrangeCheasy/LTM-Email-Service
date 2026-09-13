c4835ac191f82ed543ec4270ebf46dd3945cb8fa
Sanghyuk Mo <96196827+OrangeCheasy@users.noreply.github.com>
2026-09-13 01:13:11 -0600

    fix: add mobile notification control (#9)
    * fix: add mobile notification control to mailbox header
    
    * fix: wire notification toggle into mobile mail header
    
    * style: add mobile notification button states
    
    * style: load mobile notification controls

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./dark.css";
+import "./mobile-notifications.css";
 
 const root = document.getElementById("root");
 
